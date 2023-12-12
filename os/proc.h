67d65052d04a8646d81287989de0ff62606b22ea
iibeer <3221776095@qq.com>
2023-12-12 17:28:11 +0800

    [ch5]: 上传chapter5实验所需源码

--- os/proc.h
+++ os/proc.h
@@ -3,8 +3,12 @@
 
 #include "riscv.h"
 #include "types.h"
+#include "queue.h"
 
-#define NPROC (16)
+#define NPROC (512)
+#define FD_BUFFER_SIZE (16)
+
+struct file;
 
 // Saved registers for kernel context switches.
 struct context {
@@ -28,48 +32,35 @@ struct context {
 
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
-#define MAX_SYSCALL_NUM 500
 // Per-process state
 struct proc {
 	enum procstate state; // Process state
 	int pid; // Process ID
 	pagetable_t pagetable; // User page table
-	uint64 ustack;
+	uint64 ustack; // Virtual address of kernel stack
 	uint64 kstack; // Virtual address of kernel stack
 	struct trapframe *trapframe; // data page for trampoline.S
 	struct context context; // swtch() here to run process
 	uint64 max_page;
-	/*
-	* LAB1: you may need to add some new fields here
-	*/
-	uint64 start_cycle;
-	unsigned int syscall_times[MAX_SYSCALL_NUM];
+	struct proc *parent; // Parent process
+	uint64 exit_code;
+	struct file *files[FD_BUFFER_SIZE];
 };
 
-/*
-* LAB1: you may need to define struct for TaskInfo here
-*/
-// Be consistent with user mode
-typedef enum {
-	UnInit,
-	Ready,
-	Running,
-	Exited,
-} TaskStatus;
-
-typedef struct {
-	TaskStatus state;
-	unsigned int syscall_times[MAX_SYSCALL_NUM];
-	int time;
-} TaskInfo;
-
+int cpuid();
 struct proc *curr_proc();
 void exit(int);
 void proc_init();
 void scheduler() __attribute__((noreturn));
 void sched();
 void yield();
+int fork();
+int exec(char *);
+int wait(int, int *);
+void add_task(struct proc *);
+struct proc *pop_task();
 struct proc *allocproc();
+int fdalloc(struct file *);
 // swtch.S
 void swtch(struct context *, struct context *);
 
--- os/syscall.c
+++ os/syscall.c
@@ -1,4 +1,5 @@
 #include "syscall.h"
+#include "console.h"
 #include "defs.h"
 #include "loader.h"
 #include "syscall_ids.h"
@@ -7,7 +8,7 @@
 
 uint64 sys_write(int fd, uint64 va, uint len)
 {
-	debugf("sys_write fd = %d va = %x, len = %d", fd, va, len);
+	debugf("sys_write fd = %d str = %x, len = %d", fd, va, len);
 	if (fd != STDOUT)
 		return -1;
 	struct proc *p = curr_proc();
@@ -18,6 +19,21 @@ uint64 sys_write(int fd, uint64 va, uint len)
 		console_putchar(str[i]);
 	}
 	return size;
+}
+
+uint64 sys_read(int fd, uint64 va, uint64 len)
+{
+	debugf("sys_read fd = %d str = %x, len = %d", fd, va, len);
+	if (fd != STDIN)
+		return -1;
+	struct proc *p = curr_proc();
+	char str[MAX_STR_LEN];
+	for (int i = 0; i < len; ++i) {
+		int c = consgetc();
+		str[i] = c;
+	}
+	copyout(p->pagetable, va, str, len);
+	return len;
 }
 
 __attribute__((noreturn)) void sys_exit(int code)
@@ -32,136 +48,78 @@ uint64 sys_sched_yield()
 	return 0;
 }
 
-uint64 sys_gettimeofday(uint64 va, int _tz) // TODO: implement sys_gettimeofday in pagetable. (VA to PA)
+uint64 sys_gettimeofday(uint64 val, int _tz)
 {
-	// YOUR CODE
 	struct proc *p = curr_proc();
-	TimeVal val;
 	uint64 cycle = get_cycle();
-	val.sec = cycle / CPU_FREQ;
-	val.usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
-	copyout(p->pagetable, va, (char *)&val, sizeof(val));
+	TimeVal t;
+	t.sec = cycle / CPU_FREQ;
+	t.usec = (cycle % CPU_FREQ) * 1000000 / CPU_FREQ;
+	copyout(p->pagetable, val, (char *)&t, sizeof(TimeVal));
 	return 0;
 }
 
-// TODO: add support for mmap and munmap syscall.
-// hint: read through docstrings in vm.c. Watching CH4 video may also help.
-// Note the return value and PTE flags (especially U,X,W,R)
-int sys_mmap(uint64 va, unsigned long long len, int port, int flag, int fd) {
-	if (len == 0) {
-		return 0;
-	}
-	if (len > 1024 * 1024 * 1024) {
-		errorf("sys_mmap: the maxinum len is 1G");
-		return -1;
-	}
-	if ((va & (PAGE_SIZE - 1)) !=0 ) {
-		errorf("start address must be aligned to page size");
-		return -1;
-	}
-	if ((port & ~0x7) != 0 || (port & 0x7) == 0) {
-		errorf("not allow port: 0x%x", port);
-		return -1;
-	}
-
-	struct proc *p = curr_proc();
-	int perm = (port << 1) | PTE_U;
-	uint32 page_num = (len + PAGE_SIZE - 1) / PAGE_SIZE;
-	for (uint32 i = 0; i < page_num; i++) {
-		uint64 pa;
-		if ((pa = (uint64)kalloc()) <= 0) {
-			errorf("sys_mmap: kalloc physical page failed");
-			return -1;
-		}
-		if (mappages(p->pagetable, va + PAGE_SIZE * i,
-				PAGE_SIZE, pa, perm) != 0) {
-			errorf("sys_mmap: mappages failed");
-			return -1;
-		}
-	}
-	return 0;
-} 
-
-extern pte_t *walk(pagetable_t, uint64, int);
-int sys_munmap(uint64 va, unsigned long long len) {
-	if (len == 0) {
-		return 0;
-	}
-	if (len > 1024 * 1024 * 1024) {
-		errorf("sys_mmap: the maxinum len is 1G");
-		return -1;
-	}
-	if ((va & (PAGE_SIZE - 1)) !=0 ) {
-		errorf("start address must be aligned to page size");
-		return -1;
-	}
-
-	struct proc *p = curr_proc();
-	uint32 page_num = (len + PAGE_SIZE - 1) / PAGE_SIZE;
-	
-	pte_t *pte;
-	for (uint8 i = 0; i < page_num; i++) {
-		if ((pte = walk(p->pagetable, va + i * PAGE_SIZE, 0)) == 0) {
-			errorf("virtual address: 0x%x not have PTE", va + i * PAGE_SIZE);
-			return -1;
-		}
-		if ((*pte & PTE_V) == 0) {
-			errorf("virtual address: 0x%x PTE no mapping Physical address", va + i * PAGE_SIZE);
-			return -1;
-		}
-		if ((*pte & PTE_V) != 0) {
-			if (PTE_FLAGS(*pte) == PTE_V)
-				panic("uvmunmap: not a leaf");
-			uint64 pa = PTE2PA(*pte);
-			kfree((void *)pa);
-		}
-		*pte = 0;
-	}
-
-	return 0;
+uint64 sys_getpid()
+{
+	return curr_proc()->pid;
 }
 
-/*
-* LAB1: you may need to define sys_task_info here
-*/
-uint64 sys_task_info(uint64 va) {
+uint64 sys_getppid()
+{
 	struct proc *p = curr_proc();
-	uint64 curr_cycle = get_cycle();
-	debugf("curr_cycle = %d, start_cycle = %d", curr_cycle, p->start_cycle);
-	TaskInfo ti;
-	ti.state = Running;
-	ti.time = (curr_cycle - p->start_cycle) * 1000000 / CPU_FREQ / 1000;
-	for(uint32 i = 0; i < MAX_SYSCALL_NUM; i++) {
-		ti.syscall_times[i] = p->syscall_times[i];
-	}
-	copyout(p->pagetable, va, (char *)&ti, sizeof(ti));
-	return 0;
+	return p->parent == NULL ? IDLE_PID : p->parent->pid;
 }
+
+uint64 sys_clone()
+{
+	debugf("fork!\n");
+	return fork();
+}
+
+uint64 sys_exec(uint64 va)
+{
+	struct proc *p = curr_proc();
+	char name[200];
+	copyinstr(p->pagetable, name, va, 200);
+	debugf("sys_exec %s\n", name);
+	return exec(name);
+}
+
+uint64 sys_wait(int pid, uint64 va)
+{
+	struct proc *p = curr_proc();
+	int *code = (int *)useraddr(p->pagetable, va);
+	return wait(pid, code);
+}
+
+uint64 sys_spawn(uint64 va)
+{
+	// TODO: your job is to complete the sys call
+	return -1;
+}
+
+uint64 sys_set_priority(long long prio){
+    // TODO: your job is to complete the sys call
+    return -1;
+}
+
 
 extern char trap_page[];
 
 void syscall()
 {
-	struct proc *curr = curr_proc();
-	struct trapframe *trapframe = curr->trapframe;
+	struct trapframe *trapframe = curr_proc()->trapframe;
 	int id = trapframe->a7, ret;
 	uint64 args[6] = { trapframe->a0, trapframe->a1, trapframe->a2,
 			   trapframe->a3, trapframe->a4, trapframe->a5 };
 	tracef("syscall %d args = [%x, %x, %x, %x, %x, %x]", id, args[0],
 	       args[1], args[2], args[3], args[4], args[5]);
-	/*
-	* LAB1: you may need to update syscall counter for task info here
-	*/
-	if (id >= MAX_SYSCALL_NUM) {
-		errorf("syscall err id: %d", id);
-		ret = -1;
-		goto OUT;
-	}
-	curr->syscall_times[id]++;
-
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], args[1], args[2]);
+		break;
+	case SYS_read:
+		ret = sys_read(args[0], args[1], args[2]);
 		break;
 	case SYS_exit:
 		sys_exit(args[0]);
@@ -172,24 +130,28 @@ void syscall()
 	case SYS_gettimeofday:
 		ret = sys_gettimeofday(args[0], args[1]);
 		break;
-	/*
-	* LAB1: you may need to add SYS_taskinfo case here
-	*/
-	case SYS_task_info:
-		ret = sys_task_info(args[0]);
+	case SYS_getpid:
+		ret = sys_getpid();
 		break;
-	case SYS_mmap:
-		ret = sys_mmap(args[0], args[1], args[2], args[3], args[4]);
+	case SYS_getppid:
+		ret = sys_getppid();
 		break;
-	case SYS_munmap:
-		ret = sys_munmap(args[0], args[1]);
+	case SYS_clone: // SYS_fork
+		ret = sys_clone();
+		break;
+	case SYS_execve:
+		ret = sys_exec(args[0]);
+		break;
+	case SYS_wait4:
+		ret = sys_wait(args[0], args[1]);
+		break;
+	case SYS_spawn:
+		ret = sys_spawn(args[0]);
 		break;
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
 	}
-
-OUT:
 	trapframe->a0 = ret;
 	tracef("syscall ret %d", ret);
 }
